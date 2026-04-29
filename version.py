588ae50e40d44b5f9ae871ae4cd93429e6bd85ff
Luke <lukemdg73@gmail.com>
2026-04-29 08:43:00 +0200

    Bump version to 1.0.3

--- version.py
+++ version.py
@@ -4,4 +4,4 @@
 #   2. Commit and push a matching git tag:  git tag v1.0.1 && git push origin v1.0.1
 #   3. GitHub Actions will automatically build and publish the new release.
 
-__version__ = "1.0.1"
+__version__ = "1.0.3"
